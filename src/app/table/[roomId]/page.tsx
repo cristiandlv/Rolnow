85ac6afc2efbeedc820fda47b32e168fba61e7b9
cristiandlv <dlvcristian@gmail.com>
2025-08-20 21:54:36 -0300

    fixes vercel 2

--- src/app/table/[roomId]/page.tsx
+++ src/app/table/[roomId]/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 export const dynamic = "force-dynamic";
+
 import { useParams } from "next/navigation";
 import TablePage from "@/app/components/TablePage";
 
 export default function RoomPage() {
   const params = useParams();
   const roomId = params?.roomId as string;
+
   if (!roomId) return <p className="text-white">Cargando sala...</p>;
+
   return <TablePage roomId={roomId} />;
 }
